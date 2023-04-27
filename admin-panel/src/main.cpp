42e7e8f2bc6615f0530a9891db07e5924e62291e
DHipo <104331457+DHipo@users.noreply.github.com>
2023-04-26 23:01:31 -0300

    init wifi and server

--- admin-panel/src/main.cpp
+++ admin-panel/src/main.cpp
@@ -2,12 +2,28 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
+const char * ssid = "Coexist WiFi module";
+const char * password = "coexist123";
+
+ESP8266WebServer server(8080);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  WiFi.begin();
+  WiFi.mode(WIFI_AP);
+
+  WiFi.softAP(ssid, password);
+
+  server.on("/", [](){
+    server.send(200, "text/plain", "Hola mundo");
+  });
+  
+  server.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Hello world");
+  server.handleClient();
 }
